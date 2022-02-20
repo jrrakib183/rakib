eda57d18867c9a1cb3406dea35e8cfcc4e838e2a
bahauddin <bahauddinmnb20@gmail.com>
2022-02-20 23:58:53 +0600

    add fail

---
+++ webpack.mix.js
@@ -0,0 +1,39 @@
+const mix = require('laravel-mix');
+require('laravel-mix-nunjucks')
+const min = mix.inProduction() ? '.min' : '';
+
+// mix.webpackConfig({ devtool: "inline-source-map" }); //TODO: check it for global css source map
+
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production') {
+
+    mix.options({
+        terser: {
+            extractComments: false,
+        },
+        processCssUrls: false
+    });
+
+    mix.js('src/js/main.js', `dist/assets/js/main${min}.js`)
+        .sass('src/scss/main.scss', `dist/assets/css/main${min}.css`).sourceMaps()
+        .sass('src/scss/invoice.scss', `dist/assets/css/invoice${min}.css`).sourceMaps()
+        .sass('src/scss/invoice-single.scss', `dist/assets/css/invoice-single${min}.css`).sourceMaps()
+        .sass('src/scss/sidebar.scss', `dist/assets/css/sidebar${min}.css`).sourceMaps()
+        .sass('src/scss/common.scss', `dist/assets/css/common${min}.css`).sourceMaps()
+        .njk('src/html', 'dist/', {
+            ext: '.html',
+            marked: null,
+            watch: true,
+            block: 'content',
+            envOptions: {
+                watch: true,
+                noCache: true
+            },
+            manageEnv: (nunjucks) => { },
+        })
+        /* .postCss('src/scss/tailwind.css', `dist/assets/css/tailwind${min}.css`, [
+            require('tailwindcss'),
+        ]) */
+        .copy('src/img', 'dist/assets/img')
+        .copy('src/font', 'dist/assets/font')
+        .copy('src/vendor', 'dist/assets/vendor');
+}  
